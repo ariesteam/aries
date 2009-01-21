a8476232619cd2ae40382d798c922571ad6b14a3
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-01-21 20:11:59 +0000

    New aries demo functions
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@1876 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
@@ -1,7 +1,7 @@
 
 ;; ---------------------------------------------------------------------------------------------------
-;; test: this should run the interactive GSSM interface on the whole ARIES dataset for 
+;; test: this should run the interactive GSSM interface on the whole ARIES dataset for climate stability
 ;; ---------------------------------------------------------------------------------------------------
 
-(aries.demo/run-gssm-demo (tl/conc 'carbonService:ClimateStability))
+(aries.demo/run-gssm-demo 'carbonService:ClimateStability 128)
 (tl/alert "done")
