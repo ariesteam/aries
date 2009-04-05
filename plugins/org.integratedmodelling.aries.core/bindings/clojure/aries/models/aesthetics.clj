796856b2bd8029d13b6346becdd88d643ba02b6d
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-04-05 20:19:20 +0000

    Models are read correctly and installed by defmodel in a ModelManager so they can be retrieved on a observable concept basis.
    Aesthetic example model read by default in Aries, doesn't do much useful yet. Ontologies updated to make it compile. 
    
    Random models change names to more meaningful ones. Implementation is next.
    
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2106 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/aries/models/aesthetics.clj
@@ -0,0 +1,37 @@
+(ns aries.models
+	(:refer-clojure)
+  (:refer modelling :only (defmodel measurement classification ranking
+  			 									 random-classification noisymax-classification)))
+
+(defmodel valuable-waterbodies 'aestheticService:WaterBody
+		 (classification (ranking 'nlcd:NLCDNumeric)
+	 			23   'aestheticService:Lake
+	 			32   'aestheticService:Ocean
+	 			:otherwise 'aestheticService:NoWater))
+
+(defmodel valuable-mountain 'aestheticService:Mountain
+   "Classifies an elevation model into three levels of provision of beautiful mountains"
+   (classification  (measurement 'geophysics:Altitude "m")
+   		[:< 2000]    'aestheticService:NoMountain
+   		[2000 2750]  'aestheticService:SmallMountain 
+   		[2750 :>]    'aestheticService:LargeMountain))
+   		    		 
+(defmodel aesthetic-enjoyment-provision 'aestheticService:SensoryEnjoyment
+
+ 	 "Unconditional bayesian model of sensory enjoyment provision. Will assign probabilities to 
+ 	  4 levels of provision of aesthetic enjoyment based on altitude and NCLD numeric class
+ 	  data."
+ 	  
+ 	 (noisymax-classification 'aestheticService:SensoryEnjoyment
+ 	 
+ 	 	 ; states -- should be all the disjoint subclasses, but we don't know the ordering unless we specify
+ 	 	 ('aestheticService:NoSensoryEnjoyment 'aestheticService:LowSensoryEnjoyment 
+ 	 	 	'aestheticService:ModerateSensoryEnjoyment 'aestheticService:HighSensoryEnjoyment)	
+ 	 	 	
+	   ; cpt for the noisymax - all others default to uniform
+ 		 [0.4 0.3 0.2 0.1 0.2 0.3 0.3 0.2 0.0 0.0 0.0 1.0 0.7 0.1
+ 	 		0.1 0.1 0.5 0.2 0.2 0.1 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0])
+ 	 		
+ 	 	 :context
+  	 	 ((random-classification valuable-mountain)
+  	 	  (random-classification valuable-waterbodies)))
