cd2ee5b51c4848af2d2add21a154f68ba884a2e9
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-04-14 02:15:30 +0000

    moving models up in the dir structure
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2126 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
@@ -0,0 +1,12 @@
+
+;; ---------------------------------------------------------------------------------------------------
+;; test: this should run the interactive GSSM interface on the whole ARIES dataset for climate stability
+;; ---------------------------------------------------------------------------------------------------
+
+;  (aries.demo/make-dataset 'carbonService:ClimateStability "climate_data" 512)
+(aries.demo/run-gssm-demo 'aestheticService:SensoryEnjoyment 'aestheticService:AestheticViewshedUse 256 0.2)
+;(aries.demo/run-gssm-demo 'aestheticService:ProximityToBeauty 'aestheticService:AestheticProximityUse 64 0.2)
+;(aries.demo/run-gssm-demo 'carbonService:ClimateStability 'carbonService:AllPeopleEverywhere 256 0.2)
+;(aries.demo/run-gssm-demo 'carbonService:ClimateStability 'carbonService:GreenhouseGasEmitters 256 0.2)
+;(aries.demo/run-gssm-demo 'carbonService:ClimateStability 'carbonService:ParticularlyVulnerableGroups 256 0.2)
+(tl/alert "done")
---
+++ plugins/org.integratedmodelling.aries.core/models/aesthetics-dset.clj
@@ -0,0 +1,26 @@
+(ns aries.models
+	(:refer-clojure)
+  (:refer modelling :only (defmodel measurement classification ranking identification)))
+
+(defmodel valuable-waterbodies 'aestheticService:WaterBody
+	(classification (ranking 'nlcd:NLCDNumeric)
+	 		23   'aestheticService:Lake
+			32   'aestheticService:Ocean
+			:otherwise 'aestheticService:NoWater))
+
+(defmodel valuable-mountain 'aestheticService:Mountain
+   "Classifies an elevation model into three levels of provision of beautiful mountains"
+   (classification  (measurement 'geophysics:Altitude "m")
+   		[:< 2000]    'aestheticService:NoMountain
+   		[2000 2750]  'aestheticService:SmallMountain 
+   		[2750 :>]    'aestheticService:LargeMountain))
+   		    		 
+(defmodel view-data 'aestheticService:SensoryEnjoyment
+
+ 	 "Just the dataset for further processing. This is a test model that merely collects, transforms and
+ 	  harmonizes data."
+ 	  
+ 	 (identification 'aestheticService:SensoryEnjoyment)
+ 	 	 :context
+  	 	 (valuable-mountain    :as mountain 
+  	 	  valuable-waterbodies :as water))
