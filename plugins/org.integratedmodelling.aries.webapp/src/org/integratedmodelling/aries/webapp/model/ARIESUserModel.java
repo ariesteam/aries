28fe085cdbb6cbc1223429a01a93ba09351149b2
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-04-17 20:06:24 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2136 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.aries.webapp/src/org/integratedmodelling/aries/webapp/model/ARIESUserModel.java
@@ -0,0 +1,42 @@
+package org.integratedmodelling.aries.webapp.model;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.integratedmodelling.thinkcap.core.ThinkcapModel;
+import org.integratedmodelling.thinkcap.core.ThinkcapSession;
+import org.integratedmodelling.thinkcap.core.exceptions.ThinkcapException;
+
+public class ARIESUserModel extends ThinkcapModel {
+
+	@Override
+	public void initialize(ThinkcapSession session) throws ThinkcapException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void persist(String authenticatedUser) throws ThinkcapException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restore(String authenticatedUser) throws ThinkcapException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public InputStream getInputStream() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrintStream getOutputStream() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
