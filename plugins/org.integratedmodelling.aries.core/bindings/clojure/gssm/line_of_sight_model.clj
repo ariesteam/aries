c18bf7bdfa7292f704c667010605e0fe71482ede
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-05-11 19:29:47 +0000

    Removed debugging and changed model to use multiple cores.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2166 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/bindings/clojure/gssm/line_of_sight_model.clj
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/gssm/line_of_sight_model.clj
@@ -23,6 +23,15 @@
 			       distribute-load-over-processors)]))
 
 (defn find-viewpath
+  "Returns the sequence of all points [i j] intersected by the line
+   from provider to beneficiary.  Since this is calculated over a
+   regular integer-indexed grid, diagonal lines will be approximated
+   by lines bending at right angles along the p-to-b line.  This
+   calculation imagines the indeces of each point to be located at the
+   center of a square of side length 1.  Note that the first point in
+   each path will be the provider id, and the last will be the
+   beneficiary id.  If provider=beneficiary, the path will contain
+   only this one point."
   [provider beneficiary]
   (let [[pi pj] (:id provider)
 	[bi bj] (:id beneficiary)
@@ -65,15 +74,6 @@
 		       (for [j j-range i (get-i-range j)] [i j])))))
 
 (defn find-viewpaths
-  "Returns a sequence of paths (one for each combination of p and b),
-   where a path is represented by the sequence of all points [i j]
-   intersected by the line from p to b for each p,b pair.  Since this
-   is calculated over a regular integer-indexed grid, diagonal lines
-   will be approximated by lines bending at right angles along the
-   p-to-b line.  This calculation imagines the indeces of each point
-   to be located at the center of a square of side length 1.  Note
-   that the first point in each path will be its p, and the last will
-   be its b.  If p=b, the path will contain only this one point."
   [providers beneficiaries]
   (for [p providers b beneficiaries]
     (find-viewpath p b)))
@@ -81,14 +81,12 @@
 (defn get-valid-elevation
   [location]
   (let [elev ((:flow-features location) "Altitude")]
-    (if (> elev 55530.0) 0.0 elev)))
+    (if (> elev 55535.0) 0.0 elev)))
 
 (defn distribute-raycast!
   [provider beneficiary decay-rate location-map]
-;;  (println "Casting from" (:id provider))
   (let [source-val (force (:source provider))]
     (if (= provider beneficiary)
-;;      (do (println "Provider = Beneficiary")
       (swap! (:carrier-cache beneficiary) conj
 	     (struct service-carrier source-val [provider]))
       (let [source-elev (get-valid-elevation provider)
@@ -99,7 +97,6 @@
 	    path  (find-viewpath provider beneficiary)
 	    steps (dec (count path))
 	    dx    (/ run steps)]
-;;	(println "Path steps:" steps)
 	(loop [step 0
 	       traversed-locs []
 	       untraversed-ids path]
@@ -107,17 +104,13 @@
 	    (let [current-loc (location-map (first untraversed-ids))]
 	      (if (or (> (force (:sink current-loc)) 0.14)
 		      (== step steps))
-;;		(do (println "Hit a Sink/Use Location")
 		(swap! (:carrier-cache current-loc) conj
 		       (struct service-carrier
 			       (* source-val (Math/pow decay-rate (* step dx)))
 			       (conj traversed-locs current-loc))))
 	      (let [current-elev (get-valid-elevation current-loc)
 		    view-elev    (f (* step dx))]
-;;		(println "Current elev:" current-elev)
-;;		(println "View elev:   " view-elev)
 		(when (<= current-elev view-elev)
-;;		  (println "Recurring (STEP" (inc step) ")")
 		  (recur (inc step)
 			 (conj traversed-locs current-loc)
 			 (rest untraversed-ids)))))))))))
@@ -137,93 +130,7 @@
         beneficiaries (filter #(> (force (:use %)) 0.0) locations)]
     (println "Num Providers:" (count providers))
     (println "Num Beneficiaries:" (count beneficiaries))
-    (doseq [p (take 270 providers)]
-	(doseq [b beneficiaries]
-	    (if (distance-within-range? p b decay-rate trans-threshold)
-;;	      (do (println "PATH IN RANGE")
-	      (distribute-raycast! p b decay-rate location-map))))))
-;;	      (println "PATH OUT OF RANGE"))))))
-
-(comment
-
-(defmethod distribute-flow! "LineOfSight_Foo"
-  [_ {:keys [decay-rate trans-threshold]} location-map _ _]
-  (let [locations     (vals location-map)
-        providers     (filter #(> (force (:source %)) trans-threshold) locations)
-        beneficiaries (filter #(> (force (:use %)) 0.0) locations)]
     (distribute-load-over-processors
-     (fn [_ viewpath] (distribute-raycast! viewpath decay-rate))
-     (filter #(let [path-length (dec (count %))]
-                (and (distance-within-range? % path-length decay-rate trans-threshold)
-                     (no-elevation-interference? % path-length)))
-             (map #(map location-map %)
-                  (find-viewpaths providers beneficiaries))))))
-
-(defmethod distribute-flow! "LineOfSight_Bar"
-  [_ {:keys [decay-rate trans-threshold]} location-map _ _]
-  (println "LineOfSight Model begins...")
-  (let [locations      (vals location-map)
-        providers      (filter #(> (force (:source %)) trans-threshold) locations)
-        beneficiaries  (filter #(> (force (:use %)) 0.0) locations)
-	viewpaths      (map #(map location-map %) (find-viewpaths providers beneficiaries))
-	path-info      (map #(let [path-length (dec (count %))]
-;;;			       (println "Assessing next path " (:id (first %))
-;;;					"->" (:id (last %)) "(length " path-length ")")
-			       (let [range? (distance-within-range? % path-length
-								    decay-rate trans-threshold)
-				     elev?  (if range?
-					      (no-elevation-interference? % path-length))]
-;;;					      (println "-- OUT OF RANGE"))]
-;;;				 (println "Building hash-map...")
-				 (let [hmap (hash-map
-					     :path   %
-					     :steps  path-length
-					     :range? range?
-					     :elev?  elev?)]
-;;;				   (println "Built hash-map")
-				   hmap)))
-			    (take 3000 viewpaths))]
-    (println "Total Viewpaths Computed: " (count viewpaths))
-    (let [good-path?     #(and (:range? %) (:elev? %))
-	  good-path-info (filter good-path? path-info)
-	  bad-path-info  (filter (complement good-path?) path-info)]
-      (println "Total Viewpaths Accepted: " (count good-path-info))
-      (println "Total Viewpaths Rejected: " (count bad-path-info))
-      (println "Propagating carriers...")
-      (println "Results: " (map (fn [p] (distribute-raycast! p decay-rate)) (map :path good-path-info)))
-      (println "Done!"))))
-;;      (distribute-load-over-processors
-;;       (fn [_ viewpath] (distribute-raycast! viewpath decay-rate))
-;;       (map :path good-path-info))
-;;      (println "Distribute-Load Returned."))))
-
-(defmethod distribute-flow! "LineOfSight_Baz"
-  [_ {:keys [decay-rate trans-threshold]} location-map _ _]
-  (println "LineOfSight Model begins...")
-  (let [locations      (vals location-map)
-        providers      (filter #(> (force (:source %)) trans-threshold) locations)
-        beneficiaries  (filter #(> (force (:use %)) 0.0) locations)
-	viewpath-ids   (find-viewpaths providers beneficiaries)
-	viewpaths      (map #(map location-map %) viewpath-ids)]
-;;    (println "Num Locations:     " (count locations))
-;;    (println "Num Providers:     " (count providers))
-;;    (println "Num Beneficiaries: " (count beneficiaries))
-;;    (println "Provider I Min:    " (apply min (map (comp first :id) providers)))
-;;    (println "Provider I Max:    " (apply max (map (comp first :id) providers)))
-;;    (println "Provider J Min:    " (apply min (map (comp second :id) providers)))
-;;    (println "Provider J Max:    " (apply max (map (comp second :id) providers)))
-;;    (println "Beneficiary I Min: " (apply min (map (comp first :id) beneficiaries)))
-;;    (println "Beneficiary I Max: " (apply max (map (comp first :id) beneficiaries)))
-;;    (println "Beneficiary J Min: " (apply min (map (comp second :id) beneficiaries)))
-;;    (println "Beneficiary J Max: " (apply max (map (comp second :id) beneficiaries)))
-    (println "Num Viewpath-ids:  " (count viewpath-ids))
-    (println "Num Viewpaths:     " (count viewpaths))
-;;    (println "Some Providers:    " (take 11 (map :id providers)))
-;;    (println "Some Beneficiaries:" (take 26 (map :id beneficiaries)))
-;;    (newline)
-;;    (println "Id-seq  2691: " (nth viewpath-ids 2690))
-;;    (newline)
-;;    (println "Loc-seq 2691: " (map :id (nth viewpaths 2690)))))
-))
-
-)
+     (fn [_ [p b]] (if (distance-within-range? p b decay-rate trans-threshold)
+			(distribute-raycast! p b decay-rate location-map)))
+     (for [p (take 270 providers) b beneficiaries] [p b]))))
