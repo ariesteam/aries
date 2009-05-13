a8fbe3fa1d1f21eb26dbfce0827848e4887daeb4
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-05-13 19:06:14 +0000

    Proximity model runs sexily at res=200.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2177 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
@@ -5,36 +5,36 @@
 
 ;; run the view model
 (defn run-view-model
-	"Run the view model at the given resolution" 
-	[resolution]
-	(let [view-params 
-					{:decay-rate      0.98
-				 	 :trans-threshold 0.01
-				 	 :sink-type       :relative
-				 	 :use-type        :relative
-				 	 :benefit-type    :non-rival}]
-		(aries.demo/run-gssm-demo 
-			'aestheticService:SensoryEnjoyment
-			'aestheticService:AestheticViewshedUse
-			'aestheticService:ViewSink
-			'aestheticService:LineOfSight view-params resolution)))
+  "Run the view model at the given resolution"
+  [resolution]
+  (let [view-params
+	{:decay-rate      0.95
+	 :trans-threshold 1.0
+	 :sink-type       :relative
+	 :use-type        :relative
+	 :benefit-type    :non-rival}]
+    (aries.demo/run-gssm-demo
+     'aestheticService:SensoryEnjoyment
+     'aestheticService:AestheticViewshedUse
+     'aestheticService:ViewSink
+     'aestheticService:LineOfSight view-params resolution)))
 
 (defn run-proximity-model
-	"Run the proximity model at the given resolution" 
-	[resolution]
-	(let [view-params 
-					{:decay-rate      0.75
-				 	 :trans-threshold 0.25
-				 	 :sink-type       :relative
-				 	 :use-type        :relative
-				 	 :benefit-type    :non-rival}]
-		(aries.demo/run-gssm-demo 
-			'aestheticService:ProximityToBeauty
-			'aestheticService:AestheticProximityUse
-			'aestheticService:ProximitySink
-			'aestheticService:Proximity view-params resolution)))
+  "Run the proximity model at the given resolution" 
+  [resolution]
+  (let [view-params 
+	{:decay-rate      0.6
+	 :trans-threshold 1.0
+	 :sink-type       :relative
+	 :use-type        :relative
+	 :benefit-type    :non-rival}]
+    (aries.demo/run-gssm-demo
+     'aestheticService:ProximityToBeauty
+     'aestheticService:AestheticProximityUse
+     'aestheticService:ProximitySink
+     'aestheticService:Proximity view-params resolution)))
 
-(run-proximity-model 100)
+(run-proximity-model 200)
 
 ;(aries.demo/make-dataset 'aestheticService:ProximityToBeauty "proximity_data" 512)
 ;(aries.demo/run-gssm-demo 'aestheticService:ProximityToBeauty 'aestheticService:AestheticProximityUse 64 0.2)
