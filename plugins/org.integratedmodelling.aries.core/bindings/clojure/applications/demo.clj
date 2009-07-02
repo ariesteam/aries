ef628790c54326115ecf3bf2a226fc0e7ffec850
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-07-02 21:04:43 +0000

    Still bugs: 1) computed fields in metadata are put in the wrong place when inserting; 2) metadata have Clojure keywords as keys and have not been processed to ensure they contain IValues.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2245 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/applications/demo.clj
@@ -34,7 +34,7 @@
      'aestheticService:ProximitySink
      'aestheticService:Proximity view-params resolution)))
 
-(run-view-model 100)
+(run-view-model 200)
 
 ;(aries.demo/make-dataset 'aestheticService:ProximityToBeauty "proximity_data" 512)
 ;(aries.demo/run-gssm-demo 'aestheticService:ProximityToBeauty 'aestheticService:AestheticProximityUse 64 0.2)
