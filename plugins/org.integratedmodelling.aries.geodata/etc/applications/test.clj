1241b252c7ab6d0a85ae95103e475da9165eb21d
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-07-04 14:49:34 +0000

    test query with spatial metadata working
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2250 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.geodata/etc/applications/test.clj
+++ plugins/org.integratedmodelling.aries.geodata/etc/applications/test.clj
@@ -1,15 +1,15 @@
-(tl/query	
-
-	(modelling/kbox geodata "postgres://postgres:rnbh304@localhost:5432/geodata" 
-				:protocol "pg" 
-				:schema "postgis"
-				:metadata (
-					:country      thinklab-core:Text
-	        :gmicode      thinklab-core:Text
-	        :region       thinklab-core:Text
-	        :centroid     geospace:Point
-	        :boundingbox  geospace:Polygon)
-				:sql.use.pooling "false" 
-				:sql.log.queries "true")
-		
-		'(observation:Observation (boundingbox contains "POINT (12 32)")))
+(let 
+	[kbox (modelling/kbox geodata "postgres://postgres:rnbh304@localhost:5432/geodata" 
+					:protocol "pg" 
+					:schema "postgis"
+					:metadata (
+						:country      thinklab-core:Text
+	        	:gmicode      thinklab-core:Text
+	       	  :region       thinklab-core:Text
+	       	  :centroid     geospace:Point
+	          :boundingbox  geospace:Polygon)run g
+	        :parameters (:srid 4326)
+			  	:sql.use.pooling "false" 
+				  :sql.log.queries "true")
+		 qres (tl/query kbox '(observation:Observation (boundingbox contains "POINT (12 32)")))]
+	(println "Nres = " (.getResultCount qres)))
