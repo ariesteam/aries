04ec315e0cf7c6fa8223dbee7a40194b766bf932
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-08-23 19:50:14 +0000

    Cosmetic
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2291 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/models/aesthetics-dset.clj
+++ plugins/org.integratedmodelling.aries.core/models/aesthetics-dset.clj
@@ -3,12 +3,15 @@
   (:refer modelling :only (defmodel measurement classification ranking identification)))
 
 (defmodel valuable-waterbodies 'aestheticService:WaterBody
+
+   "Classifies a land use model into lakes, oceans and land"
 	(classification (ranking 'nlcd:NLCDNumeric)
 	 		23   'aestheticService:Lake
 			32   'aestheticService:Ocean
 			:otherwise 'aestheticService:NoWater))
 
 (defmodel valuable-mountain 'aestheticService:Mountain
+
    "Classifies an elevation model into three levels of provision of beautiful mountains"
    (classification  (measurement 'geophysics:Altitude "m")
    		[:< 2000]    'aestheticService:NoMountain
@@ -19,7 +22,6 @@
 
  	 "Just the dataset for further processing. This is a test model that merely collects, transforms and
  	  harmonizes data."
- 	  
  	 (identification 'aestheticService:SensoryEnjoyment)
  	 	 :context
   	 	 (valuable-mountain    :as mountain 
