4d14c2a665d47a55b8ccafa79239f26636e7de7d
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-09-29 08:18:17 +0000

    Renaming admin (botched SVN config) to administration
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2363 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.aries.administration/src/org/integratedmodelling/aries/administration/ARIESAdministrationPlugin.java
@@ -0,0 +1,27 @@
+package org.integratedmodelling.aries.administration;
+
+import org.integratedmodelling.thinklab.KnowledgeManager;
+import org.integratedmodelling.thinklab.exception.ThinklabException;
+import org.integratedmodelling.thinklab.plugin.ThinklabPlugin;
+
+public class ARIESAdministrationPlugin extends ThinklabPlugin {
+
+	public static final String PLUGIN_ID = "org.integratedmodelling.aries.administration";
+	
+	public static ARIESAdministrationPlugin get() {
+		return (ARIESAdministrationPlugin) getPlugin(PLUGIN_ID);
+	}
+	
+	@Override
+	protected void load(KnowledgeManager km) throws ThinklabException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void unload() throws ThinklabException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
