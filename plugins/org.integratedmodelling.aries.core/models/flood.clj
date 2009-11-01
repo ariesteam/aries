9b2d0bd7230e9d978d6b097c101e9be83a05226f
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-11-01 03:20:52 +0000

    More flood models working
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2387 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/models/flood.clj
+++ plugins/org.integratedmodelling.aries.core/models/flood.clj
@@ -3,6 +3,23 @@
   (:refer modelling :only (defmodel measurement classification categorization ranking identification bayesian)))
 
 ;; ----------------------------------------------------------------------------------------------
+;; source models
+;; ----------------------------------------------------------------------------------------------
+
+;(defmodel flood-related-land-use 'floodService:LandUseLandCover
+;	"Just a reclass of the NLCD land use layer"
+;	(classification (ranking 'nlcd:NLCDNumeric)
+;		82	               'floodService:Agriculture
+;		{11 90 95}	       'floodService:WetlandsOpenWater
+;		21	               'floodService:DevelopedOpenSpace
+;		[41 43 :inclusive] 'floodService:Forest
+;		{71 81}	           'floodService:GrassPasture
+;		22	               'floodService:DevelopedLowIntensity
+;		23	               'floodService:DevelopedMediumIntensity
+;		24	               'floodService:DevelopedHighIntensity
+;		:otherwise         'floodService:IrrelevantLandUse))
+
+;; ----------------------------------------------------------------------------------------------
 ;; use models
 ;; ----------------------------------------------------------------------------------------------
 
@@ -17,10 +34,42 @@
 	(classification (categorization 'floodService:PresenceOfHousing)
 		"RESIDENTIAL" 'floodService:HousingPresent
 		:otherwise    'floodService:HousingNotPresent))
+		
+(defmodel public-asset 'floodService:PublicAsset
+	"Public assets are defined as presence of highways or railways"
+	(ranking 'floodService:PublicAsset)
+		:state  '(if (or (eq highway 1) (eq railway 1)) 1 0)
+		:context (
+			(ranking 'infrastructure:Highway) :as highway
+			(ranking 'infrastructure:Railway) :as railway))
+			
+(defmodel farmland 'floodService:Farmland
+	"Just a reclass of the NLCD land use layer"
+	(classification (ranking 'nlcd:NLCDNumeric)
+		82	       'floodService:FarmlandPresent
+		:otherwise 'floodService:FarmlandNotPresent))
 
+;; Resident users in floodplains
 (defmodel flood-residents-use 'floodService:FloodResidentsUse
 		"Interface to Flood resident use bayesian network"
 	  (bayesian 'floodService:FloodResidentsUse)
 	  	:import   "../aries/plugins/org.integratedmodelling.aries.core/demo/bn/FloodResidentsUse.xdsl"
 	  	:keep     ('floodService:ResidentsInFloodHazardZones)
 	 	 	:context  (presence-of-housing floodplains))
+
+;; Farmer users in floodplains
+(defmodel flood-farmers-use 'floodService:FloodFarmersUse
+		"Interface to Flood farmers use bayesian network"
+	  (bayesian 'floodService:FloodFarmersUse)
+	  	:import   "../aries/plugins/org.integratedmodelling.aries.core/demo/bn/FloodFarmersUse.xdsl"
+	  	:keep     ('floodService:FarmersInFloodHazardZones)
+	 	 	:context  (farmland floodplains))
+
+;; Public assets in floodplains
+(defmodel flood-residents-use 'floodService:FloodPublicAssetsUse
+  	"Interface to Flood public asset use bayesian network"
+	  (bayesian 'floodService:FloodPublicAssetsUse)
+	  	:import   "../aries/plugins/org.integratedmodelling.aries.core/demo/bn/FloodPublicAssetsUse.xdsl"
+	  	:keep     ('floodService:PublicAssetOwnersAndUsersInFloodHazardZones)
+	 	 	:context  (public-asset floodplains))
+	 	 	
