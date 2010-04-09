2cb0a99536a1b0c6c9f6065d01da65df6c0b2847
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-04-09 20:20:44 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2744 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/models/aesthetics.clj
+++ plugins/org.integratedmodelling.aries.core/models/aesthetics.clj
@@ -23,7 +23,7 @@
 (defmodel mountain 'aestheticService:Mountain
   "Classifies an elevation model into three levels of provision of beautiful mountains"
   (classification (measurement 'geophysics:Altitude "m")
-		  [2000 2750]  'aestheticService:SmallMountain ; 
+		  [1000 2750]  'aestheticService:SmallMountain ; 
 		  [2750 8850]  'aestheticService:LargeMountain ; no higher than mount Everest!
 		  :otherwise   'aestheticService:NoMountain ; will catch artifacts too
 		  ))
