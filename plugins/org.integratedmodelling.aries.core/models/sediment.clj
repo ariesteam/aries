8533a29c7e7770c8dc8dfcd3e4144d596b2a8633
ken_bagstad <ken_bagstad@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-05-06 01:49:06 +0000

    Updates to sediment.clj
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2815 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/models/sediment.clj
+++ plugins/org.integratedmodelling.aries.core/models/sediment.clj
@@ -6,49 +6,158 @@
 ;; provision model
 ;; ----------------------------------------------------------------------------------------------
 
-(defmodel lake 'aestheticService:Lake
-  "Just being a lake. We may want to reclass lake area instead"
-  (classification (ranking 'geofeatures:Lake)
-		  0          'aestheticService:LakeAbsent
-		  :otherwise 'aestheticService:LakePresent))
+(defmodel soil-group 'floodService:HydrologicSoilsGroup
+	"Relevant soil group"
+	(classification (categorization 'floodService:HydrologicSoilsGroup)
+			"A"        'floodService:SoilGroupA
+			"B"        'floodService:SoilGroupB
+			"C"        'floodService:SoilGroupC
+			"D"        'floodService:SoilGroupD))
 
-(defmodel ocean 'aestheticService:Ocean
-  "Just being there."
-  (classification (ranking 'geofeatures:Ocean)
-		  0          'aestheticService:OceanAbsent
-		  :otherwise 'aestheticService:OceanPresent))
+(defmodel slope 'floodService:Slope
+		(classification (ranking 'geophysics:DegreeSlope)
+			 :units       "degrees" 
+			 [:< 1.15] 	  'floodService:Level
+			 [1.15 4.57] 	'floodService:GentlyUndulating
+			 [4.57 16.70] 'floodService:RollingToHilly
+			 [16.70 :>] 	'floodService:SteeplyDissectedToMountainous))
 
-(defmodel mountain 'aestheticService:Mountain
-  "Classifies an elevation model into three levels of provision of beautiful mountains"
-  (classification (measurement 'geophysics:Altitude "m")
-		  [1000 2750]  'aestheticService:SmallMountain ; 
-		  [2750 8850]  'aestheticService:LargeMountain ; no higher than mount Everest!
-		  :otherwise   'aestheticService:NoMountain ; will catch artifacts too
-		  ))
+(defmodel slope-stability 'soilretentionEcology:SlopeStability
+    (classification (ranking 'soilretentionEcology:SlopeStability)	 		
+      1           'soilretentionEcology:HighSlopeStability
+	 		2           'soilretentionEcology:ModerateSlopeStability
+	 		3           'soilretentionEcology:LowSlopeStability)) 
 
-(defmodel theoretical-beauty 'aestheticService:TheoreticalNaturalBeauty
-	(classification 'aestheticService:TheoreticalNaturalBeauty
-  		[0 25]   'aestheticService:NoNaturalBeauty 
-  		[25 50]  'aestheticService:LowNaturalBeauty 
-  		[50 75]  'aestheticService:ModerateNaturalBeauty 
-  		[75 100] 'aestheticService:HighNaturalBeauty))
+(defmodel soil-texture 'soilretentionEcology:SoilTexture
+    (classification (categorization 'soilretentionEcology:SoilTexture)
+      "Coarse"    'soilretentionEcology:CoarseSoilTexture
+      "Medium"    'soilretentionEcology:MediumSoilTexture
+      "Fine"      'soilretentionEcology:FineSoilTexture)) 
 
-;; source bayesian model	    		 
+(defmodel precipitation 'floodService:Precipitation
+	"FIXME this is total monthly precipitation I believe."
+	(classification (measurement 'habitat:Precipitation "in")
+		[:< 3] 	  'floodService:VeryLowPrecipitation
+		[3 6] 	  'floodService:LowPrecipitation
+		[6 12] 	  'floodService:ModeratePrecipitation
+		[12 24] 	'floodService:HighPrecipitation
+		[24 :>] 	'floodService:VeryHighPrecipitation))
+
+;; surface temperature - again, should be monthly and matched by temporal extents.
+(defmodel monthly-temperature 'floodService:MonthlyTemperature
+		(classification (ranking 'geophysics:GroundSurfaceTemperature "C")
+			 [4 :>] 	'floodService:HighTemperature
+			 [-4 4] 	'floodService:ModerateTemperature
+			 [:< -4] 	'floodService:LowTemperature))
+
+;;GARY: is my notation right below for 0 & 0-50?
+(defmodel monthly-snowmelt 'soilretentionEcology:MonthlySnowmelt
+	(classification (measurement 'soilretentionEcology:MonthlySnowmelt "mm/month")
+		[0] 	    'soilretentionEcology:NoSnowmelt
+		[0 50] 	  'soilretentionEcology:LowSnowmelt
+		[50 100] 	'soilretentionEcology:ModerateSnowmelt
+		[100 :>] 	'soilretentionEcology:HighSnowmelt))
+
+;;This is annual runoff, whereas snowmelt, precipitation, and temperature are monnthly, so this is problematic.
+;;Could divide yearly runoff by 12 but obviously it's not evenly distributed throughout the year.
+(defmodel runoff 'soilretentionEcology:Runoff
+	(classification (measurement 'soilretentionEcology:Runoff "mm/yr")
+		[0 200] 	    'soilretentionEcology:VeryLowRunoff
+		[200 600] 	  'soilretentionEcology:LowRunoff
+		[600 1200]  	'soilretentionEcology:ModerateRunoff
+		[1200 2400] 	'soilretentionEcology:HighRunoff
+		[2400 :>] 	  'soilretentionEcology:VeryHighRunoff))
+
+(defmodel vegetation-type 'soilretentionEcology:VegetationType
+	"Just a reclass of the NLCD land use layer"
+	(classification (ranking 'nlcd:NLCDNumeric)
+		#(21 22 23 24 31 82) 'soilretentionEcology:CropsBarrenDeveloped
+		#{41 42 43 71 90 95} 'soilretentionEcology:ForestGrasslandWetland
+		#(52 81)             'soilretentionEcology:ShrublandPasture))
+
+(defmodel percent-vegetation-cover 'floodService:PercentVegetationCover
+	(classification (ranking 'habitat:PercentCanopyCover)
+		[80 :>] 'floodService:VeryHighVegetationCover
+		[60 80] 'floodService:HighVegetationCover
+		[40 60] 'floodService:ModerateVegetationCover
+		[20 40] 'floodService:LowVegetationCover
+		[0 20]  'floodService:VeryLowVegetationCover))
+
+(defmodel successional-stage 'floodService:SuccessionalStage
+	 (classification (ranking 'ecology:SuccessionalStage)
+	 		#{5 6}      'floodService:OldGrowth
+	 		4           'floodService:LateSuccession
+	 		3           'floodService:MidSuccession
+	 		2           'floodService:EarlySuccession
+	 		1           'floodService:PoleSuccession
+	 		:otherwise  'floodService:NoSuccession))
+
+;;We have data for the top node, which is great, but it's from 1980 at the global level, so not
+;;temporally consistent.  I'm also unclear on how to use conditional statements to model the process
+;;with different discretizations and model components in Puget Sound, Madagascar, and the DR.
+;;Discretization below is for Puget Sound; discretization will look different for DR & Mg
+(defmodel sediment-source-value 'soilretentionEcology:SedimentSourceValue
+	(classification 'soilretentionEcology:SedimentSourceValue
+  		[0]             'soilretentionEcology:NoSedimentSource
+  		[0 30000]       'soilretentionEcology:LowSedimentSource 
+  		[30000 100000]  'soilretentionEcology:ModerateSedimentSource
+  		[100000 :>]     'soilretentionEcology:HighSedimentSource))
+
+;; source bayesian model for Puget Sound   
+;; I'm unclear about what to replace "AestheticEnjoymentProvision" and "theoretical-beauty" with since 
+;; they're different from the name of the top node. 		 
 (defmodel source 'aestheticService:AestheticEnjoymentProvision
   "This one will harmonize the context, then retrieve and run the BN with the given
    evidence, and produce a new observation with distributions for the requested nodes."
   (bayesian 'aestheticService:AestheticEnjoymentProvision 
-    :import   "aries.core::ViewSource.xdsl"
-    :keep     ('aestheticService:TheoreticalNaturalBeauty)
-    :context  (mountain lake ocean)
+    :import   "aries.core::SedimentSourceValueAdHoc.xdsl"
+    :keep     ('soilretentionEcology:SedimentSourceValue
+    :context  (soil-group slope soil-texture slope-stability precipitation monthly-snowmelt 
+              monthly-temperature runoff vegetation-type percent-vegetation-cover successional-stage)
     :observed (theoretical-beauty)))
+
+;;BELOW is another piece of the model to use in Mg & DR:
+
+(defmodel storm-probability 'coastalProtection:TropicalStormProbability
+	(ranking 'habitat:TropicalStormProbability
+      [0]     'habitat:NoTropicalStormProbability
+      [1 5]   'habitat:ModerateTropicalStormProbability
+      [6 10]  'habitat:HighTropicalStormProbability))
+
+;;Discretization of top node for DR & Mg (could revisit values - see what Lee & Nathlay come up with)
+(defmodel sediment-source-value 'soilretentionEcology:SedimentSourceValue
+	(classification 'soilretentionEcology:SedimentSourceValue
+      [0]        'soilretentionEcology:NoSedimentSource
+  		[0 15]     'soilretentionEcology:LowSedimentSource 
+  		[15 40]    'soilretentionEcology:ModerateSedimentSource
+  		[40 :>]    'soilretentionEcology:HighSedimentSource))
+
+;; source bayesian model for DR & Mg
+;; I'm unclear about what to replace "AestheticEnjoymentProvision" and "theoretical-beauty" with since 
+;; they're different from the name of the top node. 
+;; Need to add successional stage if Lee & Nathaly are going to use it for the DR: it won't be used for Mg		 
+(defmodel source 'aestheticService:AestheticEnjoymentProvision
+  "This one will harmonize the context, then retrieve and run the BN with the given
+   evidence, and produce a new observation with distributions for the requested nodes."
+  (bayesian 'aestheticService:AestheticEnjoymentProvision 
+    :import   "aries.core::SedimentSourceValueMgAdHoc"
+    :keep     ('soilretentionEcology:SedimentSourceValue
+    :context  (soil-group slope soil-texture precipitation storm-probability 
+              runoff vegetation-type percent-vegetation-cover)
+    :observed (theoretical-beauty)))
+
+;; Add deterministic model for USLE: Have data for it for the western U.S. and world in 1980.
 
 ;; ----------------------------------------------------------------------------------------------
 ;; use model
 ;; ----------------------------------------------------------------------------------------------
 
-;;STILL NEED DEFMODELS FOR hydro reservoirs + all components of fisheries BNs
-;; In the long run, could take 2 paths: 1) ditch fisheries BNs & use source/use models for actual fisheries; 2) use BNs as generalized fisheries impact model.
+;;THIS IS FOR PUGET SOUND: DO WE WANT A SEPARATE CLOJURE FILE FOR MG?  SEPARATE MODELS?  SAME MODELS?
+;;GARY THOUGHTS?
+
+;;STILL NEED DEFMODELS FOR all components of fisheries BNs.  In the long run, could take 2 paths:
+;; 1) ditch fisheries BNs & use source/use models for actual fisheries
+;; 2) use BNs as generalized fisheries impact model.
 
 (defmodel floodplains 'floodService:Floodplain
 	(classification (ranking 'floodService:Floodplains)
@@ -135,17 +244,11 @@ defmodel fishermen-use 'soilretentionEcology:FishermenUse
 		  0          'soilretentionEcology:LeveeAbsent
 		  :otherwise 'soilretentionEcology:LeveePresent))
 
-;; STRONGLY consider removing bridges - discuss with Brian - they don't actually narrow the floodplain...
-(defmodel bridges 'soilretentionEcology:Bridges 
-  (classification (ranking 'soilretentionEcology:Bridges)
-		  0          'soilretentionEcology:BridgeAbsent
-		  :otherwise 'soilretentionEcology:BridgePresent))
-
 (defmodel sink 'soilretentionEcology:SedimentSink
   (bayesian 'soilretentionEcology:SedimentSink 
     :import  "aries.core::SedimentSink.xdsl"
     :keep    ('soilretentionEcology:SedimentSink)
-    :context (reservoirs stream-gradient floodplain-vegetation-cover floodplain-width levees bridges)))
+    :context (reservoirs stream-gradient floodplain-vegetation-cover floodplain-width levees)))
 
 ;; ----------------------------------------------------------------------------------------------
 ;; dependencies for the flow model
