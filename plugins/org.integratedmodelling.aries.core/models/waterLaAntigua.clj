b63208de53be0c2625ceaf2c6736716aa2c7f75f
ken_bagstad <ken_bagstad@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-05-24 15:21:58 +0000

    La Antigua model included
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@2936 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.aries.core/models/waterLaAntigua.clj
@@ -0,0 +1,169 @@
+(ns aries.waterLaAntigua
+  (:refer modelling :only (defmodel measurement classification categorization ranking identification bayesian))
+  (:refer aries :only (span)))
+
+;; ----------------------------------------------------------------------------------------------
+;; provision model
+;; ----------------------------------------------------------------------------------------------
+
+;;Runoff would be preferable to precipitation, but it's at a very coarse spatial resolution
+;; (i.e., <3 full pixels for the La Antigua watershed.  Use precip for now, with the goal of 
+;; incorporating a better runoff model.
+(defmodel precipitation-annual 'soilretentionEcology:AnnualPrecipitation
+	"FIXME this is annual precipitation."
+	(classification (measurement 'soilretentionEcology:AnnualPrecipitation "mm")
+    [:< 600] 	    'soilretentionEcology:VeryLowAnnualPrecipitation
+		[600 1200] 	  'soilretentionEcology:LowAnnualPrecipitation
+		[1200 1800]   'soilretentionEcology:ModerateAnnualPrecipitation
+		[1800 2200] 	'soilretentionEcology:HighAnnualPrecipitation
+		[2200 :>] 	  'soilretentionEcology:VeryHighAnnualPrecipitation))
+
+;;Incorporate runoff data in the future once we've done a better job with the hydro modeling.
+;;(defmodel runoff 'soilretentionEcology:AnnualRunoff
+	;;(classification (measurement 'soilretentionEcology:Runoff "mm/year")
+		;;[0 200] 	    'soilretentionEcology:VeryLowAnnualRunoff
+		;;[200 600] 	  'soilretentionEcology:LowAnnualRunoff
+		;;[600 1200]  	'soilretentionEcology:ModerateAnnualRunoff
+		;;[1200 2400] 	'soilretentionEcology:HighAnnualRunoff
+		;;[2400 :>] 	  'soilretentionEcology:VeryHighAnnualRunoff))
+
+;;Water supply source value
+(defmodel sediment-source-value-annual 'soilretentionEcology:SedimentSourceValueAnnual
+	(classification (measurement 'soilretentionEcology:SedimentSourceValueAnnual "t/ha")
+      0                     'soilretentionEcology:NoAnnualSedimentSource
+  		[:exclusive 0 15]     'soilretentionEcology:LowAnnualSedimentSource 
+  		[15 40]               'soilretentionEcology:ModerateAnnualSedimentSource
+  		[40 :>]               'soilretentionEcology:HighAnnualSedimentSource))
+
+;; ----------------------------------------------------------------------------------------------
+;; use model
+;; ----------------------------------------------------------------------------------------------
+
+(defmodel floodplains 'soilretentionEcology:Floodplains
+	(classification (ranking 'soilretentionEcology:Floodplains)
+			0 'soilretentionEcology:NotInFloodplain
+			1 'soilretentionEcology:InFloodplain))
+
+(defmodel farmland 'soilretentionEcology:Farmland
+	"Just a reclass of the regionally appropriate LULC layer"
+	(classification (ranking 'nlcd:NLCDNumeric)
+		82	       'soilretentionEcology:Farmland
+		:otherwise 'soilretentionEcology:Farmland)
+  (classification (ranking 'corine:CORINENumeric)
+		[12 22 :inclusive]	 'soilretentionEcology:Farmland
+		:otherwise           'soilretentionEcology:Farmland)
+  (classification (ranking 'mglulc:MGLULCNumeric)
+		#{11 12 13} 'soilretentionEcology:Farmland
+		:otherwise 'soilretentionEcology:Farmland)
+  (classification (ranking 'soilretentionEcology:Farmland)
+    1          'soilretentionEcology:FarmlandPresent
+    0          'soilretentionEcology:FarmlandAbsent) 
+;;Above statement (soilretentionEcology:Farmland) is for coffee farmers in the DR; to use farmland 
+;; from DR LULC data, comment out the above and turn on the statement below (domlulc:DOMLULCNumeric)
+;;(classification (ranking 'domlulc:DOMLULCNumeric)
+		;;#{23 36 38 40 41 45 53 59}	'soilretentionEcology:Farmland
+		;;:otherwise                'soilretentionEcology:Farmland)
+	(classification (ranking 'glc:GLCNumeric)
+		#{16 17 18} 'soilretentionEcology:Farmland
+		:otherwise 'soilretentionEcology:Farmland))
+
+;;Use normal dam storage (ac-ft in the U.S. or m^3 in the rest of the world) as a proxy for 
+;;hyroelectric generation capacity (use) - in reality dam height & flow are important factors but 
+;;we don't have flow data.
+(defmodel hydroelectric-use-level 'soilretentionEcology:HydroelectricUseLevel
+  (measurement 'soilretentionEcology:HydroelectricUseLevel "m^3"))
+
+;;Reservoirs use for DR: presence/absence only.
+(defmodel hydroelectric-use-presence 'soilretentionEcology:HydroelectricUsePresence
+	(ranking 'soilretentionEcology:HydroelectricUsePresence))
+
+;; Models farmland in the floodplain, the non-Bayesian way (i.e., basic spatial overlap).
+(defmodel farmers-deposition-use-dr 'soilretentionEcology:DepositionProneFarmers 
+  (ranking 'soilretentionEcology:DepositionProneFarmers
+       :state #(if (and (= (:floodplains %) 1.0)
+                        (= (:farmlandpresent %) 1.0))
+                    1
+                    0)
+       :context (
+          (ranking 'soilretentionEcology:Farmland :as farmlandpresent)
+          (ranking 'soilretentionEcology:Floodplains :as floodplains)))) 
+
+;; Models farmland in regions with erodible soils, the non-Bayesian way (i.e., basic spatial overlap).
+(defmodel farmers-erosion-use-dr 'soilretentionEcology:ErosionProneFarmers
+  (ranking 'soilretentionEcology:ErosionProneFarmers
+       :state #(if (= (:farmlandpresent %) 1.0)
+                  (cond (= (:sediment-source-value-annual %) 'soilretentionEcology:ModerateAnnualSedimentSource)
+                        1
+                        (= (:sediment-source-value-annual %) 'soilretentionEcology:HighAnnualSedimentSource)
+                        2
+                        :otherwise
+                        0)
+                  0)
+       :context ((ranking 'soilretentionEcology:Farmland :as farmlandpresent))))
+
+;;Still need defmodels for all components of fisheries BNs.  What about deterministic nodes?
+;;Need an undiscretization defmodel before this, for the "observed"? In the long run, could take 2 paths:
+;; 1) ditch fisheries BNs & use source/use models for actual fisheries
+;; 2) use BNs as generalized fisheries impact model.
+;;(defmodel fishermen-use-puget 'soilretentionEcology:FishermenUse 
+	  ;;(bayesian 'soilretentionEcology:FishermenUse  
+	 ;; 	:import   "aries.core::SedimentFishermenUse.xdsl"
+	 ;; 	:keep     ('soilretentionEcology:FishermenUse)
+	 ;;	 	:context  (lakes rivers coastline coastal-wetlands salmon-spawning-grounds public-access population-density)))
+
+;;defmodel fishermen-use-mg 'soilretentionEcology:FishermenUse 
+	;;  (bayesian 'soilretentionEcology:FishermenUse  
+	 ;; 	:import   "aries.core::SedimentFishermenUseMg.xdsl"
+	 ;; 	:keep     ('soilretentionEcology:FishermenUse)
+	 ;;	 	:context  (lakes rivers coastline coastal-wetlands mangroves reefs seagrass population-density)))
+
+;; ----------------------------------------------------------------------------------------------
+;; sink model
+;; ----------------------------------------------------------------------------------------------
+
+;;At this point we're naively saying there are no sinks for suface water.  This model should
+;;be defined and implemented in the future.
+
+;; ----------------------------------------------------------------------------------------------
+;; dependencies for the flow model
+;; ----------------------------------------------------------------------------------------------
+
+;;Everything below needs to be updated correctly for sediment.
+ 	 								
+;;(defmodel altitude 'geophysics:Altitude
+  ;;(measurement 'geophysics:Altitude "m"))	 								
+ 
+;; ---------------------------------------------------------------------------------------------------	 	 	
+;; overall models 
+;; ---------------------------------------------------------------------------------------------------	 	 	
+
+;; all data, for testing and storage
+ ;;(defmodel data 'aestheticService:AestheticEnjoyment 
+	;;(identification 'aestheticService:AestheticEnjoyment)
+		;;  :context (
+		;;	source :as source
+		;;	homeowners :as use
+		;;	sink :as sink
+		;;	altitude :as altitude))
+			
+;; the real enchilada
+;;(defmodel view 'aestheticService:AestheticView
+ ;; (span 'aestheticService:LineOfSight 
+  	;;    'aestheticService:TheoreticalNaturalBeauty
+  	;;    'aestheticService:HomeownerViewUse
+    ;;  	'aestheticService:TotalVisualBlight
+    ;;  	'aestheticService:View
+  	;;    'geophysics:Altitude
+  ;; 	:sink-type        :relative
+  ;; 	:use-type         :relative
+  ;; 	:benefit-type     :non-rival
+  ;; 	:downscaling-factor 3
+  ;; 	:rv-max-states    10 
+  ;;  :context
+    ;;     (source homeowners sink altitude
+    ;;     (ranking 'eserv:SourceThreshold :value 50)
+    ;;     (ranking 'eserv:SinkThreshold :value 0.3)
+    ;;     (ranking 'eserv:UseThreshold :value 0.1)
+    ;;     (ranking 'eserv:TransitionThreshold :value 1.0))
+;;))
+
