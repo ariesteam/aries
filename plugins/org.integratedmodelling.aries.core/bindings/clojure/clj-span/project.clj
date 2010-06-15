72d8347ea352362c4fb4a0d6e724cd6f19690a69
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-06-15 17:51:42 +0000

    Removing clj-span toplevel from aries.core plugin.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@3107 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/bindings/clojure/clj-span/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject org.clojars.lambdatronic/clj-span "1.0.0-alpha"
-  :description "clj-span is an implementation of the Service Path
-                Attribution Networks (Springer LNCS 2010 - Johnson et
-                al.) framework for Ecosystem Service Assessment."
-  :dependencies [[org.clojure/clojure "1.0.0"]
-		 [org.clojure/clojure-contrib "1.0.0"]]
-  :dev-dependencies [[org.clojars.jwr/lein-swank "1.1.0"]
-		     [lein-clojars "0.5.0"]]
-  :namespaces   [clj-span.commandline])
