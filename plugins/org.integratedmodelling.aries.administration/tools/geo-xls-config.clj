72aca887520053e20443304dc478106ac7c1a044
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-11-24 01:03:50 +0000

    Adding the geo-xls Geoserver REST updater script to the admin plugin's tools/ directory.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@3337 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.aries.administration/tools/geo-xls-config.clj
@@ -0,0 +1,17 @@
+{
+  :spreadsheet-filename  "/home/gjohnson/code/clojure/projects/geo-xls/resources/Geoserver_REST_database_combined.xls"
+  :spreadsheet-sheetname "Sheet1"
+  :column-spec           {:A :Workspace
+                          :B :Store
+                          :D :Layer
+                          :E :Description
+                          :F :URI
+                          :G :DefaultStyle
+                          :K :NativeSRS
+                          :L :DeclaredSRS}
+  :namespace-prefix      "http://www.integratedmodelling.org/geo/ns/"
+  :geoserver-rest-uri    "http://ecoinformatics.uvm.edu/geoserver/rest"
+  :geoserver-username    "admin"
+  :geoserver-password    "rnbh304"
+  :geoserver-data-dir    "/mnt/raid/geodata/"
+}
