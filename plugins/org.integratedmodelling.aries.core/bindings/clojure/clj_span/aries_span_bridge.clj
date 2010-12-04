c60f8d8a1213326f45650d7555500d0dd86de862
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-12-04 21:39:03 +0000

    Setup clj-span.aries-span-bridge to attach the Hydrosheds layer to the flow-layers map for the SurfaceWaterMovement flow model.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@3377 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/bindings/clojure/clj_span/aries_span_bridge.clj
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/clj_span/aries_span_bridge.clj
@@ -258,7 +258,7 @@
           sink-layer   (layer-from-observation observation sink-concept   rows cols)
           use-layer    (layer-from-observation observation use-concept    rows cols)
           flow-layers  (let [layer-map (layer-map-from-observation observation flow-concept rows cols)]
-                         (if (#{"Sediment" "FloodWaterMovement"} flow-model)
+                         (if (#{"Sediment" "FloodWaterMovement" "SurfaceWaterMovement"} flow-model)
                            (assoc layer-map "Hydrosheds" (get-hydrosheds-layer observation rows cols))
                            layer-map))]
       (println "Cell Dimensions in meters:" [w h] "\n")
