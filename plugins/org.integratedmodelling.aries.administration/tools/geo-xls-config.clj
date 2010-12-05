a46f3517eec5a6904000e6f647a9c015fc6f28fe
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-12-05 20:51:28 +0000

    options configured to run on huginn
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@3383 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.administration/tools/geo-xls-config.clj
+++ plugins/org.integratedmodelling.aries.administration/tools/geo-xls-config.clj
@@ -1,5 +1,5 @@
 {
-  :spreadsheet-filename  "/home/gjohnson/code/clojure/projects/geo-xls/resources/Geoserver_REST_database.xls"
+  :spreadsheet-filename  "../etc/catalog/Geoserver_REST_database.xls"
   :spreadsheet-sheetname "Sheet1"
   :column-spec           {:Workspace    :A
                           :Store        :B
@@ -8,10 +8,10 @@
                           :URI          :F
                           :DefaultStyle :G
                           :NativeSRS    :K
-                          :DeclaredSRS  :L}
+                          :DeclaredSRS  :L}Ec
   :namespace-prefix      "http://www.integratedmodelling.org/geo/ns/"
   :geoserver-rest-uri    "http://ecoinformatics.uvm.edu/geoserver/rest"
   :geoserver-username    "admin"
   :geoserver-password    nil
-  :geoserver-data-dir    "/mnt/raid/geodata/"
+  :geoserver-data-dir    "/raid/geodata/"
 }
