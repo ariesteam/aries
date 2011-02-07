f35009fcd82ce89020a0b9dd421a31a97bc12575
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2011-02-07 15:53:19 +0000

    Units with modifiers and currency units with working implementation (as long as CPI conversion is working of course). Inroads in Sextante integration.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@3617 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.core/models/test.clj
+++ plugins/org.integratedmodelling.aries.core/models/test.clj
@@ -13,45 +13,44 @@
 ;(defmodel altitude-mm Altitude
 ;	(measurement geophysics:Altitude "mm"))
 
-(defmodel conservation-status 'conservation:ProtectedStatus
-	(ranking 'conservation:ProtectedStatus))
+(defmodel conservation-status conservation:ProtectedStatus
+	(ranking conservation:ProtectedStatus))
 	
-(defmodel categories 'floodService:PresenceOfHousing
-	(categorization 'floodService:PresenceOfHousing))
+(defmodel categories floodService:PresenceOfHousing
+	(categorization floodService:PresenceOfHousing))
 	
-(defmodel altitude-m 'geophysics:Altitude
-  (measurement 'geophysics:Altitude "m")
+(defmodel altitude-m geophysics:Altitude
+  (measurement geophysics:Altitude "m")
   :as a1)
   
-(defmodel altitude-ft 'geophysics:Altitude
-  (measurement 'geophysics:Altitude "ft"))
+(defmodel altitude-ft geophysics:Altitude
+  (measurement geophysics:Altitude "ft"))
 
-(defmodel slope 'geophysics:Slope
-  (measurement 'geophysics:DegreeSlope "\u00b0")
-  :as a2)
+(defmodel slope geophysics:Slope
+  (modelling.gis/slope (measurement geophysics:Altitude "m")))
 
-(defmodel idall 'representation:GenericQuantifiable
-	(identification 'representation:GenericQuantifiable
+(defmodel idall representation:GenericQuantifiable
+	(identification representation:GenericQuantifiable
 		:context(altitude-m slope))) 
 
-(defmodel idran 'representation:GenericQuantifiable
-	(ranking 'representation:GenericQuantifiable
+(defmodel idran representation:GenericQuantifiable
+	(ranking representation:GenericQuantifiable
 		:state #(+ (:a1 %) (:a2 %))
 		:context(altitude-m slope))) 
 
-(defmodel idfuc 'representation:GenericQuantifiable
-	(ranking 'representation:GenericQuantifiable
+(defmodel idfuc representation:GenericQuantifiable
+	(ranking representation:GenericQuantifiable
 		:state #(+ (:a1 %) (:a2 %))
 		:context (idall)))
 
-(defmodel lulc 'lulc:LandClassificationNumericMapping
-  (numeric-coding 'nlcd:NLCDNumeric)
-  (numeric-coding 'corine:CORINENumeric)
-  (numeric-coding 'mglulc:MGLULCNumeric)
-  (numeric-coding 'domlulc:DOMLULCNumeric)
-  (numeric-coding 'glc:GLCNumeric))
+(defmodel lulc lulc:LandClassificationNumericMapping
+  (numeric-coding nlcd:NLCDNumeric)
+  (numeric-coding corine:CORINENumeric)
+  (numeric-coding mglulc:MGLULCNumeric)
+  (numeric-coding domlulc:DOMLULCNumeric)
+  (numeric-coding glc:GLCNumeric))
 
-(defmodel altitude-computed 'geophysics:Altitude 
+(defmodel altitude-computed geophysics:Altitude 
 
  "Test state computation. When the observation is mediating another
   model (in this case, implicitly, whatever compatible observation of altitude was seen), the 
@@ -60,14 +59,14 @@
   If the :as clause is not given, the state will be available as the local name of the observed
   concept (in this case, Altitude)."
 
-  (measurement 'geophysics:Altitude "m"
+  (measurement geophysics:Altitude "m"
     :as    altitude 
     :state #(+ 100000000 (:altitude %))
    )) 
 
 ;; simple test of dynamic updating
-(defmodel dynamic 'representation:GenericQuantifiable 
-  (measurement 'representation:GenericQuantifiable "m"
+(defmodel dynamic representation:GenericQuantifiable 
+  (measurement representation:GenericQuantifiable "m"
     :value  (corescience/gaussian 150.0 3.75)
     :as     self
     :update #(do  
@@ -77,8 +76,8 @@
 
 
 ;; simple test of ODE integration. This should grow exponentially.
-(defmodel ode 'representation:GenericQuantifiable
-  (ranking 'representation:GenericQuantifiable
+(defmodel ode representation:GenericQuantifiable
+  (ranking representation:GenericQuantifiable
     :value  50
     :as     self
     :rate #(do  
@@ -87,40 +86,40 @@
               (* (:self %) 0.03)) 
 )) 
 
-(defmodel farmland 'floodService:Farmland
-	(classification (numeric-coding 'nlcd:NLCDNumeric)
-			82	       'floodService:FarmlandPresent
-			:otherwise 'floodService:FarmlandAbsent))
+(defmodel farmland floodService:Farmland
+	(classification (numeric-coding nlcd:NLCDNumeric)
+			82	       floodService:FarmlandPresent
+			:otherwise floodService:FarmlandAbsent))
 			
 ;; cellular automaton contagion model - farmland only survives to next generation if surrounded by farmland
 ;; on three sides, meaning it will erode at the edges at each update until all farmland patches are square.
-(defmodel land-use-change 'floodService:Farmland
+(defmodel land-use-change floodService:Farmland
 	(classification 
-		(binary-coding 'nlcd:NLCDNumeric
+		(binary-coding nlcd:NLCDNumeric
       :as nlcd   
       :state #(if (= (:nlcd %) 82) 1 0))
-   	0 'floodService:FarmlandPresent
-    1 'floodService:FarmlandAbsent
+   	0 floodService:FarmlandPresent
+    1 floodService:FarmlandAbsent
     :update #(let [sum (tl/apply-not-nil + (:nlcd#n %) (:nlcd#s %) (:nlcd#e %) (:nlcd#w %))]
     			       (if (< sum 3) (tl/conc 'floodService:FarmlandAbsent) (tl/conc 'floodService:FarmlandPresent)))))
 
 ;; test structural variability
-(defmodel structest 'conservation:ProtectedStatus 
+(defmodel structest conservation:ProtectedStatus 
 
- [(ranking 'nlcd:NLCDNumeric :as landuse)] 
+ [(ranking nlcd:NLCDNumeric :as landuse)] 
 
-	(measurement 'geophysics:Altitude "ft" 
+	(measurement geophysics:Altitude "ft" 
      :when #(contains? #{41.0 42.0 43.0} (:landuse %)))
 
   ;; you can just say :value 40.0 (or any compatible object) or use a distribution from
   ;; corescience/ssj.clj
-  (measurement 'geophysics:Altitude "m" :value (corescience/gaussian 150.0 3.75))
+  (measurement geophysics:Altitude "m" :value (corescience/gaussian 150.0 3.75))
 )
 
-(defscenario eroded 'geophysics:Altitude 
+(defscenario eroded geophysics:Altitude 
   "Altitude is eroded by 100 m. Try it anywhere altitude is needed."
-  (model 'geophysics:Altitude
-    (measurement 'geophysics:Altitude "m"
+  (model geophysics:Altitude
+    (measurement geophysics:Altitude "m"
       :as    altitude 
       :state #(- (:altitude %) 100.0))))
 
@@ -143,20 +142,17 @@
 ;; agents
 ;; -------------------------------------------------------------------------
 
-(defagent rock-climber 'carbonService:Emitter 
+(defagent test-agent carbonService:Emitter 
 
-  "A rock climber whose model of the world consists of altitude, slope and imperviousness (so
-   s/he knows where to pee). It should also have a model of self. And it emits carbon for obvious
-   reasons."
-  (measurement 'geophysics:Altitude "m" :as altitude)
-  (ranking 'habitat:PercentImperviousness :as imperviousness)
-  (classification (ranking 'geophysics:DegreeSlope)
+  (measurement geophysics:Altitude "m" :as altitude)
+  (ranking habitat:PercentImperviousness :as imperviousness)
+  (classification (ranking geophysics:DegreeSlope)
     :units       "degrees" 
     :as          slope
-    [:< 1.15] 	 'floodService:Level
-    [1.15 4.57]  'floodService:GentlyUndulating
-    [4.57 16.70] 'floodService:RollingToHilly
-    [16.70 :>] 	 'floodService:SteeplyDissectedToMountainous)
+    [:< 1.15] 	 floodService:Level
+    [1.15 4.57]  floodService:GentlyUndulating
+    [4.57 16.70] floodService:RollingToHilly
+    [16.70 :>] 	 floodService:SteeplyDissectedToMountainous)
      
   ;; these are supposed to be rules                                                          
   :update #(if (> (:altitude %) 4000) (.die %))
