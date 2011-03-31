2a2a11fac08b92e57426075b93e3c46f41bac08d
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2011-03-31 21:05:54 +0000

    Reformatting the hideousness of load-data.clj
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/aries@3717 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.aries.administration/etc/applications/load-data.clj
+++ plugins/org.integratedmodelling.aries.administration/etc/applications/load-data.clj
@@ -2,37 +2,33 @@
   (:refer tl :only (with-kbox kbox get-plugin-resource get-property-value))
   (:refer geospace :only (get-centroid get-bounding-box)))
 
-(with-kbox 
-	
-	(kbox aries-kbox "postgres://postgres:rnbh304@localhost:5432/ariesdata" 
-				:protocol "pg" 
-				:schema "postgis"
-				:metadata (
-	        :centroid     geospace:Point
-	        :boundingbox  geospace:Polygon
-          :dataset      thinklab-core:Text)
-				:sql.log.queries "true"
-				:kbox.parameter.srid "4326")
-				
-	  ;; rebuild the db from scratch every time this is run
-		:storage-policy :recreate-always
+(with-kbox
 
-		;; put the kbox definition in the load area of the main plugin so it will be loaded at startup
-		:persist org.integratedmodelling.aries.aries
-		
-		:metadata-generator {
-			:centroid    #(get-centroid %)
-			:boundingbox #(get-bounding-box %)
-      :dataset     #(get-property-value % "metadata:belongsToDataset")
-		}
-		
-		(import (get-plugin-resource 'aries.administration "ark.xml"))
-		(import (get-plugin-resource 'aries.administration "common.xml"))
-		(import (get-plugin-resource 'aries.administration "ipcc.xml"))
-		(import (get-plugin-resource 'aries.administration "nlcd2001.xml"))
-		(import (get-plugin-resource 'aries.administration "agri.xml"))
-		(import (get-plugin-resource 'aries.administration "marine.xml"))
-		(import (get-plugin-resource 'aries.administration "kb1.xml"))
-		(import (get-plugin-resource 'aries.administration "bv1.xml"))
-		(import (get-plugin-resource 'aries.administration "raven_ridge_viewsheds.xml")) 
-	)
+  (kbox aries-kbox "postgres://postgres:rnbh304@localhost:5432/ariesdata"
+        :protocol "pg"
+        :schema "postgis"
+        :metadata (:centroid    geospace:Point
+                   :boundingbox geospace:Polygon
+                   :dataset     thinklab-core:Text)
+        :sql.log.queries "true"
+        :kbox.parameter.srid "4326")
+
+  ;; rebuild the db from scratch every time this is run
+  :storage-policy :recreate-always
+
+  ;; put the kbox definition in the load area of the main plugin so it will be loaded at startup
+  :persist org.integratedmodelling.aries.aries
+
+  :metadata-generator {:centroid    #(get-centroid %)
+                       :boundingbox #(get-bounding-box %)
+                       :dataset     #(get-property-value % "metadata:belongsToDataset")}
+
+  (import (get-plugin-resource 'aries.administration "ark.xml"))
+  (import (get-plugin-resource 'aries.administration "common.xml"))
+  (import (get-plugin-resource 'aries.administration "ipcc.xml"))
+  (import (get-plugin-resource 'aries.administration "nlcd2001.xml"))
+  (import (get-plugin-resource 'aries.administration "agri.xml"))
+  (import (get-plugin-resource 'aries.administration "marine.xml"))
+  (import (get-plugin-resource 'aries.administration "kb1.xml"))
+  (import (get-plugin-resource 'aries.administration "bv1.xml"))
+  (import (get-plugin-resource 'aries.administration "raven_ridge_viewsheds.xml")))
