5efb897efde637c4260762b1af5244c0e8291f09
Gary Johnson <gwjohnso@uvm.edu>
2011-06-28 23:02:24 -0400

    The tweaking continues...

--- plugins/org.integratedmodelling.aries.core/models/aesthetic_proximity_puget.clj
+++ plugins/org.integratedmodelling.aries.core/models/aesthetic_proximity_puget.clj
@@ -192,7 +192,7 @@
         :sink-type          :infinite
         :use-type           :infinite
         :benefit-type       :non-rival
-        :downscaling-factor 4
+        :downscaling-factor 1
         :rv-max-states      10
         :animation?         false
         ;;:save-file          (str (System/getProperty "user.home") "/aesthetic_proximity_puget_data.clj")
