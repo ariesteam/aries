dfd49e7883172b0a2f895960f1e578b5dda2173d
kbagstad <kbagstad@gmail.com>
2011-07-11 13:49:53 -0600

    another C bug fix

--- plugins/org.integratedmodelling.aries.core/contexts/san-pedro.clj
+++ plugins/org.integratedmodelling.aries.core/contexts/san-pedro.clj
@@ -288,8 +288,8 @@
 ;; Changes to carbon models
   (transform 'carbonService:PercentVegetationCover 'carbonService:VeryLowVegetationCover
      mesquite_sc_1 mesquite_sc_2 mesquite_sc_3))
-;;  (transform 'carbonService:HardwoodSoftwoodRatio 'carbonService:LowHardness
-;;     mesquite_sc_1 mesquite_sc_2 mesquite_sc_3))
+  (transform 'sanPedro:CarbonVegetationType 'sanPedro:Grassland
+     mesquite_sc_1 mesquite_sc_2 mesquite_sc_3))
 
 (defcontext mesquite-base
    "Baseline to compare mesquite results at the same spatial extent and resolution"
