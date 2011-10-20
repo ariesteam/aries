1d2d9f2d29c8cd097a6a1367c26643aad9ddd761
kbagstad <kbagstad@gmail.com>
2011-10-20 15:32:11 -0600

    Minor updates to discretization of san pedro models

--- plugins/org.integratedmodelling.aries.core/models/carbon_san_pedro.clj
+++ plugins/org.integratedmodelling.aries.core/models/carbon_san_pedro.clj
@@ -65,7 +65,7 @@
     #{52 109}                                                                          sanPedro:MesquiteWoodland
     #{62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 90 93}                              sanPedro:Grassland
     #{19 39 40 42 43 44 46 47 48 49 50 51 53 54 55 56 57 58 59 60 61 94 95 96 105 108} sanPedro:DesertScrub
-    #{77 78 79 80 81 83 84 85 98 109 110 118}                                          sanPedro:Riparian
+    #{77 78 79 80 81 83 84 85 98 118}                                                  sanPedro:Riparian
     #{1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 20 21 110 111 112 114}              sanPedro:UrbanBarrenWaterAgriculture))
 ;;  (classification (categorization mexico:CONABIOLULCCategory)
 ;;    #{"Bosque de coniferas distintas a Pinus" "Bosque de pino"}                  sanPedro:Forest
