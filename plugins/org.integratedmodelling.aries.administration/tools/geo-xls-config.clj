b7c0e5959d19857dd71b97c662da30f61595e192
Gary Johnson <gwjohnso@uvm.edu>
2011-12-14 17:28:08 -0500

    Adding copyright headers to all source files in the ARIES repository.

--- plugins/org.integratedmodelling.aries.administration/tools/geo-xls-config.clj
+++ plugins/org.integratedmodelling.aries.administration/tools/geo-xls-config.clj
@@ -1,3 +1,19 @@
+;;; Copyright 2010 Gary W. Johnson (lambdatronic@gmail.com)
+;;;
+;;; This file is part of geo-xls.
+;;;
+;;; geo-xls is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation, either version 3 of the License,
+;;; or (at your option) any later version.
+;;;
+;;; geo-xls is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with geo-xls.  If not, see <http://www.gnu.org/licenses/>.
 {
   :spreadsheet-filename  "../etc/catalog/Geoserver_REST_database.xls"
   :spreadsheet-sheetname "Sheet1"
--- plugins/org.integratedmodelling.aries.core/bindings/clojure/clj_span/model_lang.clj
+++ plugins/org.integratedmodelling.aries.core/bindings/clojure/clj_span/model_lang.clj
@@ -1,3 +1,22 @@
+;;; Copyright 2010 Gary Johnson
+;;;
+;;; This file is part of clj-span.
+;;;
+;;; clj-span is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation, either version 3 of the License,
+;;; or (at your option) any later version.
+;;;
+;;; clj-span is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with clj-span.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;;-------------------------------------------------------------------
+;;;
 ;;; This is a multipart issue: 
 ;;;
 ;;;  - Factor SPAN out of ARIES/Thinklab to build and run as an independent project (clj-span.jar). 
