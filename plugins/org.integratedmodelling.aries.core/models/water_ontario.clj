a8e72da66aacc7675fbd772403d60a3f536cf94a
Gary Johnson <gwjohnso@uvm.edu>
2012-02-08 16:47:50 -0500

    Adding template model files for Ontario project.

---
+++ plugins/org.integratedmodelling.aries.core/models/water_ontario.clj
@@ -0,0 +1,66 @@
+;;; Copyright 2011 The ARIES Consortium (http://www.ariesonline.org)
+;;;
+;;; This file is part of ARIES.
+;;;
+;;; ARIES is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; ARIES is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with ARIES.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;;-------------------------------------------------------------------
+;;;
+;;; Water supply model for Ontario
+;;;
+;;; Valid Contexts: core.contexts.ontario/*
+;;;
+;;;-------------------------------------------------------------------
+
+(ns core.models.water-ontario
+  (:refer-clojure :rename {count length})
+  (:refer tl :only [is? conc])
+  (:refer modelling :only [defscenario namespace-ontology model
+                           defmodel measurement classification
+                           categorization ranking numeric-coding
+                           probabilistic-measurement
+                           probabilistic-classification
+                           probabilistic-ranking binary-coding
+                           identification bayesian count])
+  (:refer aries :only [span]))
+
+(namespace-ontology waterSupplyService)
+
+;;;-------------------------------------------------------------------
+;;; Source models
+;;;-------------------------------------------------------------------
+
+;;;-------------------------------------------------------------------
+;;; Sink models
+;;;-------------------------------------------------------------------
+
+;;;-------------------------------------------------------------------
+;;; Use models
+;;;-------------------------------------------------------------------
+
+;;;-------------------------------------------------------------------
+;;; Routing models
+;;;-------------------------------------------------------------------
+
+;;;-------------------------------------------------------------------
+;;; Identification models
+;;;-------------------------------------------------------------------
+
+;;;-------------------------------------------------------------------
+;;; Flow models
+;;;-------------------------------------------------------------------
+
+;;;-------------------------------------------------------------------
+;;; Scenarios
+;;;-------------------------------------------------------------------
