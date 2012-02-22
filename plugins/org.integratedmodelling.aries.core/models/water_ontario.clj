76dd28c374b5dabd2c3e517a139979e4e5d0639c
brian.voigt <brian.voigt@uvm.edu>
2012-02-22 00:25:35 -0500

    Initial steps of the development of the surface water model for the Ontario case study.

--- plugins/org.integratedmodelling.aries.core/models/water_ontario.clj
+++ plugins/org.integratedmodelling.aries.core/models/water_ontario.clj
@@ -40,14 +40,86 @@
 ;;;-------------------------------------------------------------------
 ;;; Source models
 ;;;-------------------------------------------------------------------
+;; Annual precipitation interpolated from the National Climate Data
+;; and Information Archive.
+;; http://climate.weatheroffice.gc.ca/climateData/canada_e.html
+(defmodel precipitation-annual AnnualPrecipitation
+  (measurement habitat:AnnualPrecipitation "mm"))
 
+;; Annual snowfall interpolated from the National Climate Data
+;; and Information Archvie.
+;; http://climate.weatheroffice.gc.ca/climateData/canada_e.html
+;; Snowfall is converted to its rainfall equivalent so that it can
+;; be summed with the AnnualPrecipitation value to estimate
+;; TotalPrecipitation.
+
+(defmodel snowfall-annual AnnualSnowfall
+  (measurement habitat:AnnualSnowmelt "cm"))
+
+;; Total Precipitation as the sum of AnnualPrecipitation and
+;; AnnualSnowfall.
+(defmodel runoff AnnualRunoff
+  (measurement habitat:AnnualRunoff "mm"
+    :context [precipitation-annual snowfall-annual]
+    :state #(+ (:precipitation-annual %)
+               (or (:snowfall-annual  %) 0.0))))
 ;;;-------------------------------------------------------------------
 ;;; Sink models
 ;;;-------------------------------------------------------------------
+;; Still need to deal with land cover types: 4, 6, 24 28, 29
+;; need to check if these land cover classes are present
+;; in the Lake of the Woods region
+;;(defmodel land-cover-type ontario:WaterSupplyVegetationType
+;;  "Reclass of MNR Land cover data"
+;;  (classification (numeric-coding ontario:MNRLC)
+;;  #{1 2}                                                    ontario:Water
+;;  #{25 27}                                                  ontario:Agriculture
+;;  #{3 5}                                                    ontario:UrbanInfrastructureRock
+;;  #{15 16 17 18 19 20 21 22 23}                             ontario:BogFenMarshSwamp
+;;  #{9 10 11 12 13}                                          ontario:Forest
+;;  #{7 8}                                                    ontario:ImpairedForest))
+
+(defmodel percent-canopy-cover PercentTreeCanopyCoverClass
+  (classification (ranking habitat:PercentTreeCanopyCover)
+    [80 100 :inclusive]   VeryHighCanopyCover
+    [60 80]               HighCanopyCover
+    [30 60]               ModerateCanopyCover
+    [ 5 30]               LowCanopyCover
+    [ 0  5]               VeryLowCanopyCover))
+
+;;(defmodel soil-drainage HydrologicSoilsGroup
+;;  "this data is problematic because there is only a single polygon with data"
+;;(classification (numeric-coding waterSupplyService:HydrologicSoilsGroup)
+;;  1                  SoilGroupA
+;;  2                  SoilGroupB
+;;  3                  SoilGroupC
+;;  4                  SoilGroupD))
+
+(defmodel percent-impervious-cover PercentImperviousCoverClass
+  (classification (ranking habitat:PercentImperviousSurface)
+    [80 100 :inclusive] VeryHighImperviousCover
+    [50 80]             HighImperviousCover
+    [20 50]             ModeratelyHighImperviousCover
+    [10 20]             ModeratelyLowImperviousCover
+    [ 5 10]             LowImperviousCover
+    [ 0  5]             VeryLowImperviousCover))
+
+(defmodel slope SlopeClass
+  (classification (measurement geophysics:DegreeSlope "\u00B0")
+    [    0  1.15]         Level
+    [ 1.15  4.57]         GentlyUndulating
+    [ 4.57 16.70]         RollingToHilly
+    [16.70 90 :inclusive] SteeplyDissectedToMountainous))
 
 ;;;-------------------------------------------------------------------
 ;;; Use models
 ;;;-------------------------------------------------------------------
+
+;; RESIDENTIAL
+(defmodel residential-surface-water-use ResidentialSurfaceWaterUse
+  (measurement ResidentialSurfaceWaterUse "mm" ;;This is an annual value
+    :context [(count policytarget:PopulationDensity "/km^2")]
+    :state   #(* 0.8 0.082855 (:population-density %))))
 
 ;;;-------------------------------------------------------------------
 ;;; Routing models
@@ -56,6 +128,12 @@
 ;;;-------------------------------------------------------------------
 ;;; Identification models
 ;;;-------------------------------------------------------------------
+
+(defmodel altitude geophysics:Altitude
+  (measurement geophysics:Altitude "m"))
+
+(defmodel streams-simple geofeatures:River
+  (binary-coding geofeatures:River))
 
 ;;;-------------------------------------------------------------------
 ;;; Flow models
