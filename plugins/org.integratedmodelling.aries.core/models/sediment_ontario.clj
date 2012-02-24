01d122896151462baa09150e80e5f47bed120ef2
Gary Johnson <gwjohnso@uvm.edu>
2012-02-24 13:02:25 -0500

    Adding complete source model for core.models.sediment-ontario.

--- plugins/org.integratedmodelling.aries.core/models/sediment_ontario.clj
+++ plugins/org.integratedmodelling.aries.core/models/sediment_ontario.clj
@@ -41,22 +41,125 @@
 ;;; Source models
 ;;;-------------------------------------------------------------------
 
-(defmodel soil-drainage ontario:SoilDrainageClass
-  "Soil drainage rating."
+(declare annual-sediment-source
+         annual-sediment-source-class
+         sediment-vegetation-type
+         successional-stage
+         percent-tree-canopy-cover-class
+         annual-runoff-class
+         annual-runoff
+         annual-precipitation
+         annual-snowmelt
+         annual-evapotranspiration
+         slope-class
+         soil-drainage-class)
+
+;; lulc2000_low
+(defmodel sediment-vegetation-type ontario:SedimentVegetationType
+  (classification (numeric-coding ontario-lulc:MNRLULCNumeric)
+    #{4 5} ontario:ForestGrasslandWetland
+    #{6}   ontario:ShrublandPasture
+    #{2 3} ontario:CropsBarrenDeveloped))
+
+;; successional_stage_low
+(defmodel successional-stage SuccessionalStage
+  (classification (ranking ecology:SuccessionalStage)
+    6 OldGrowth
+    5 LateSuccession
+    4 MidSuccession
+    3 PoleSuccession
+    2 EarlySuccession
+    1 NoSuccession))
+
+;; canopy_low
+(defmodel percent-tree-canopy-cover-class ontario:PercentTreeCanopyCoverClass
+  (classification (ranking habitat:PercentTreeCanopyCover)
+    5 ontario:VeryHighCanopyCover
+    4 ontario:HighCanopyCover
+    3 ontario:ModerateCanopyCover
+    2 ontario:LowCanopyCover
+    1 ontario:VeryLowCanopyCover))
+
+;; precipitation_low
+(defmodel annual-precipitation AnnualPrecipitation
+  (measurement habitat:AnnualPrecipitation "mm"))
+
+;; snow_low
+(defmodel annual-snowmelt AnnualSnowmelt
+  (measurement habitat:AnnualSnowmelt "mm"))
+
+;; evapotranspiration_low
+(defmodel annual-evapotranspiration AnnualEvapotranspiration
+  (measurement habitat:Evapotranspiration "mm"))
+
+(defmodel annual-runoff AnnualRunoff
+  (measurement AnnualRunoff "mm"
+    :context [annual-precipitation
+              annual-snowmelt
+              annual-evapotranspiration]
+    :state   #(let [p (or (:annual-precipitation %)      0.0)
+                    s (or (:annual-snowmelt %)           0.0)
+                    e (or (:annual-evapotranspiration %) 0.0)]
+                (- (+ p s) e))))
+
+(defmodel annual-runoff-class ontario:AnnualRunoffClass
+  (classification annual-runoff
+    [2400   :>] ontario:VeryHighMeanAnnualRunoff
+    [1800 2400] ontario:HighMeanAnnualRunoff
+    [1200 1800] ontario:ModerateMeanAnnualRunoff
+    [ 600 1200] ontario:LowMeanAnnualRunoff
+    [   0  600] ontario:VeryLowMeanAnnualRunoff))
+
+;; slope20m_low
+(defmodel slope-class SlopeClass
+  (classification (measurement geophysics:DegreeSlope "\u00b0")
+    [    0  1.15] Level
+    [ 1.15  4.57] GentlyUndulating
+    [ 4.57 16.70] RollingToHilly
+    [16.70    :>] SteeplyDissectedToMountainous))
+
+;; soil_drainage_low
+(defmodel soil-drainage-class ontario:SoilDrainageClass
   (classification (ranking ontario:SoilDrainageCode)
     2 ontario:PoorlyDrainedSoils))
+
+(defmodel annual-sediment-source-class AnnualSedimentSourceClass
+  (probabilistic-measurement AnnualSedimentSourceClass "t/ha"
+    [100.00 300.00] HighAnnualSedimentSource
+    [ 30.00 100.00] ModerateAnnualSedimentSource
+    [  0.01  30.00] LowAnnualSedimentSource
+    [  0.00   0.01] NoAnnualSedimentSource))
+
+(defmodel annual-sediment-source AnnualSedimentSource
+  (bayesian AnnualSedimentSource
+    :import   "aries.core::SedimentSourceOntarioAdHoc.xdsl"
+    :context  [sediment-vegetation-type
+               successional-stage
+               percent-tree-canopy-cover-class
+               annual-runoff-class
+               slope-class
+               soil-drainage-class]
+    :required [ontario:SedimentVegetationType]
+    :keep     [AnnualSedimentSourceClass]
+    :result   annual-sediment-source-class))
 
 ;;;-------------------------------------------------------------------
 ;;; Sink models
 ;;;-------------------------------------------------------------------
 
+;; CapturedSediment = f(Slope,Dams,StreamGradient)
+
 ;;;-------------------------------------------------------------------
 ;;; Use models
 ;;;-------------------------------------------------------------------
 
+;; SedimentUsers = f(PopulationDensity,Dams)
+
 ;;;-------------------------------------------------------------------
 ;;; Routing models
 ;;;-------------------------------------------------------------------
+
+;; SedimentRouting = f(DEM,Hydrography)
 
 ;;;-------------------------------------------------------------------
 ;;; Identification models
@@ -69,12 +172,3 @@
 ;;;-------------------------------------------------------------------
 ;;; Scenarios
 ;;;-------------------------------------------------------------------
-
-"
-1. Create concept ontario:HydrologicSoilsGroup with child classes:
-    SoilGroupA
-    SoilGroupB
-    SoilGroupC
-    SoilGroupD
-
-"
