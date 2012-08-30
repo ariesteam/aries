06c0584edf886f32c6ec46e7d1e4cf83a149b05b
kbagstad <kbagstad@gmail.com>
2012-08-30 17:23:45 -0600

    Getting closer on fixing up the REST & xml.

--- plugins/org.integratedmodelling.aries.core/models/carbon_puget.clj
+++ plugins/org.integratedmodelling.aries.core/models/carbon_puget.clj
@@ -69,7 +69,7 @@
     3                                MidSuccession
     2                                PoleSuccession
     1                                EarlySuccession
-    #{21 22 23 24 25 26 27 28 40 41} NoSuccession))
+    #{21 22 23 24 25 26 27 28 40 41} NoSuccession)) ;; add 105?
 
 (defmodel percent-canopy-cover PercentTreeCanopyCoverClass
   (classification (ranking habitat:PercentTreeCanopyCover :units "%")
