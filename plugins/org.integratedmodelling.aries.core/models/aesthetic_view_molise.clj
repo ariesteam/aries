d9e1121740ca114b2a03432adb9b91f2c2182a59
brian.voigt <brian.voigt@uvm.edu>
2012-10-15 05:58:40 -0400

    adjusted CPTs to reflect my understanding of priorities in Molise; changed downscaling factor in model statement to equal 2

--- plugins/org.integratedmodelling.aries.core/models/aesthetic_view_molise.clj
+++ plugins/org.integratedmodelling.aries.core/models/aesthetic_view_molise.clj
@@ -196,7 +196,7 @@
         :sink-type          :infinite
         :use-type           :infinite
         :benefit-type       :non-rival
-        :downscaling-factor 1
+        :downscaling-factor 2
         :rv-max-states      10
         :animation?         false
         ;; :save-file          (str (System/getProperty "user.home") "/aesthetic_ontario_data.clj")
